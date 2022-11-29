4e87df7a59c9df78df01dd205e024716ffc56016
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-11-29 21:18:58 +0500

    emailValid

--- users/validators.py
+++ users/validators.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from django.core.exceptions import ValidationError
+from rest_framework import serializers
 
 
 def check_age(date_of_birth: date):
@@ -10,3 +11,13 @@ def check_age(date_of_birth: date):
         raise ValidationError(f"Age is {age}, it may not be less than 9")
 
 
+class EmailDomainValidator:
+    def __init__(self, domains):
+        if not isinstance(domains, list):
+            domains = [domains]
+        self.domains = domains
+
+    def __call__(self, email):
+        domain = email.split('@')[1]
+        if damain in self.domains:
+            raise serializers.ValidationError(f"Domain couldn't be {domain}")
